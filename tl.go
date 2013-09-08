d1498d013cb9d4222b525e62ac6df1bb8c4b22eb
Scott Worley <scottworley@scottworley.com>
2013-09-08 03:35:39 -0700

    gofmt

--- tl.go
+++ tl.go
@@ -9,76 +9,76 @@ import "strconv"
 import "strings"
 import "time"
 
-const daylength = 86400  // TODO: daylight savings time
+const daylength = 86400 // TODO: daylight savings time
 
 type Day string
 
 type Event struct {
-  Name string
-  Time int
+	Name string
+	Time int
 }
 
 func read_data_file(in io.Reader) (days []Day, events map[Day]([]Event)) {
-  events = make(map[Day]([]Event))
-  lines := bufio.NewScanner(in)
-  for lines.Scan() {
-    fields := strings.SplitN(lines.Text(), " ", 7)
-    day := Day(fields[0] + fields[1] + fields[2])
-    hour, hour_err := strconv.Atoi(fields[3])
-    minute, minute_err := strconv.Atoi(fields[4])
-    second, second_err := strconv.Atoi(fields[5])
-    if hour_err != nil || minute_err != nil || second_err != nil {
-      panic("Malformed line")
-    }
-    time := hour * 3600 + minute * 60 + second
+	events = make(map[Day]([]Event))
+	lines := bufio.NewScanner(in)
+	for lines.Scan() {
+		fields := strings.SplitN(lines.Text(), " ", 7)
+		day := Day(fields[0] + fields[1] + fields[2])
+		hour, hour_err := strconv.Atoi(fields[3])
+		minute, minute_err := strconv.Atoi(fields[4])
+		second, second_err := strconv.Atoi(fields[5])
+		if hour_err != nil || minute_err != nil || second_err != nil {
+			panic("Malformed line")
+		}
+		time := hour*3600 + minute*60 + second
 
-    if len(days) == 0 || days[len(days)-1] != day {
-      days = append(days, day)
-    }
-    events[day] = append(events[day], Event{fields[6], time})
-  }
-  if err := lines.Err(); err != nil {
-    panic(err)
-  }
-  return
+		if len(days) == 0 || days[len(days)-1] != day {
+			days = append(days, day)
+		}
+		events[day] = append(events[day], Event{fields[6], time})
+	}
+	if err := lines.Err(); err != nil {
+		panic(err)
+	}
+	return
 }
 
 func hash_color(title string) string {
-  if title == "" {
-    return "white"
-  }
-  hash := sha1.New()
-  io.WriteString(hash, title)
-  hue := 360.0 * int(hash.Sum(nil)[0]) / 256.0
-  return "hsl(" + strconv.Itoa(hue) + ",90%,45%)"
+	if title == "" {
+		return "white"
+	}
+	hash := sha1.New()
+	io.WriteString(hash, title)
+	hue := 360.0 * int(hash.Sum(nil)[0]) / 256.0
+	return "hsl(" + strconv.Itoa(hue) + ",90%,45%)"
 }
 
 func summarize_time(duration int) string {
-  if duration > 3600 {
-    return fmt.Sprintf("%.1f hours", float32(duration) / 3600)
-  }
-  if duration > 60 {
-    return fmt.Sprintf("%.1f min", float32(duration) / 60)
-  }
-  return fmt.Sprintf("%d sec", duration)
+	if duration > 3600 {
+		return fmt.Sprintf("%.1f hours", float32(duration)/3600)
+	}
+	if duration > 60 {
+		return fmt.Sprintf("%.1f min", float32(duration)/60)
+	}
+	return fmt.Sprintf("%d sec", duration)
 }
 
 func print_event(duration int, title string) string {
-  height := 100 * float32(duration) / daylength
-  color := hash_color(title)
-  return fmt.Sprint("<div class='event_outer' style='background-color: ", color,
-            "; height: ", height, "%'><div class='event_inner'><span title='", title,
-            " (", summarize_time(duration), ")'>", title, "</span></div></div>")
+	height := 100 * float32(duration) / daylength
+	color := hash_color(title)
+	return fmt.Sprint("<div class='event_outer' style='background-color: ", color,
+		"; height: ", height, "%'><div class='event_inner'><span title='", title,
+		" (", summarize_time(duration), ")'>", title, "</span></div></div>")
 }
 
 func generate_report(days []Day, events map[Day]([]Event)) string {
-  now_time := time.Now()
-  now := now_time.Hour() * 3600 + now_time.Minute() * 60 + now_time.Second()
-  today := Day(fmt.Sprintf("%04d%02d%02d", now_time.Year(), now_time.Month(), now_time.Day()))
-  output := fmt.Sprint(`<html><head><style type='text/css'>
+	now_time := time.Now()
+	now := now_time.Hour()*3600 + now_time.Minute()*60 + now_time.Second()
+	today := Day(fmt.Sprintf("%04d%02d%02d", now_time.Year(), now_time.Month(), now_time.Day()))
+	output := fmt.Sprint(`<html><head><style type='text/css'>
       body { margin: 0; padding: 0; height: 100% }
       html, day { margin: 0; padding: 0; height: 100% }
-      .day { width: `, 100.0 / float32(len(days)), `%; float: left }
+      .day { width: `, 100.0/float32(len(days)), `%; float: left }
       .event_outer { overflow: hidden; margin: 0 }
       .event_inner { height: 100%; padding: 2px; border: thin solid black;
                      font-family: Verdana, Geneva, sans-serif; 
@@ -100,27 +100,27 @@ func generate_report(days []Day, events map[Day]([]Event)) string {
       }
     </script></head><body>`)
 
-  prevname := ""
-  for _, day := range days {
-    output += "<div class='day'>"
-    prevtime := 0
-    for _, event := range events[day] {
-      output += print_event(event.Time - prevtime, prevname)
-      prevtime = event.Time
-      prevname = event.Name
-    }
-    if day == today {
-      output += print_event(now - prevtime, prevname)
-    } else {
-      output += print_event(daylength - prevtime, prevname)
-    }
-    output += "</div>"
-  }
-  output += "</body></html>"
-  return output
+	prevname := ""
+	for _, day := range days {
+		output += "<div class='day'>"
+		prevtime := 0
+		for _, event := range events[day] {
+			output += print_event(event.Time-prevtime, prevname)
+			prevtime = event.Time
+			prevname = event.Name
+		}
+		if day == today {
+			output += print_event(now-prevtime, prevname)
+		} else {
+			output += print_event(daylength-prevtime, prevname)
+		}
+		output += "</div>"
+	}
+	output += "</body></html>"
+	return output
 }
 
 func main() {
-  days, events := read_data_file(os.Stdin)
-  fmt.Print(generate_report(days, events))
+	days, events := read_data_file(os.Stdin)
+	fmt.Print(generate_report(days, events))
 }
