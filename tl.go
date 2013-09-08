232230c2767e8aaf8d4364e87652805f900b0bb0
Scott Worley <scottworley@scottworley.com>
2013-09-08 03:47:36 -0700

    Move <head> into the template.

--- tl.go
+++ tl.go
@@ -20,7 +20,8 @@ type Event struct {
 }
 
 type TemplateData struct {
-	HTML template.HTML
+	Body template.HTML
+	DayWidth float32
 }
 
 func read_data_file(in io.Reader) (days []Day, events map[Day]([]Event)) {
@@ -76,35 +77,12 @@ func print_event(duration int, title string) string {
 		" (", summarize_time(duration), ")'>", title, "</span></div></div>")
 }
 
-func generate_report(days []Day, events map[Day]([]Event)) TemplateData {
+func generate_report(days []Day, events map[Day]([]Event)) (td TemplateData) {
 	now_time := time.Now()
 	now := now_time.Hour()*3600 + now_time.Minute()*60 + now_time.Second()
 	today := Day(fmt.Sprintf("%04d%02d%02d", now_time.Year(), now_time.Month(), now_time.Day()))
-	output := fmt.Sprint(`<html><head><style type='text/css'>
-      body { margin: 0; padding: 0; height: 100% }
-      html, day { margin: 0; padding: 0; height: 100% }
-      .day { width: `, 100.0/float32(len(days)), `%; float: left }
-      .event_outer { overflow: hidden; margin: 0 }
-      .event_inner { height: 100%; padding: 2px; border: thin solid black;
-                     font-family: Verdana, Geneva, sans-serif; 
-                     color: white; font-weight: bold }
-    </style>
-    <script type="text/javascript">
-      document.onkeypress = function(e) {
-        e = e || window.event;
-        var dir; var scale;
-        switch(e.keyCode) {
-          case 122: document.body.style.height = document.body.style.width = "100%"; return;
-          case 107: case 119:  dir = "height"; factor = 9/10; break;
-          case 106: case 115:  dir = "height"; factor = 10/9; break;
-          case 104: case  97:  dir = "width"; factor = 9/10; break;
-          case 108: case 100:  dir = "width"; factor = 10/9; break;
-          default: return;
-        }
-        document.body.style[dir] = (parseInt(document.body.style[dir] || "100%") * factor) + "%";
-      }
-    </script></head><body>`)
-
+	td.DayWidth = 100.0/float32(len(days))
+	output := ""
 	prevname := ""
 	for _, day := range days {
 		output += "<div class='day'>"
@@ -121,8 +99,8 @@ func generate_report(days []Day, events map[Day]([]Event)) TemplateData {
 		}
 		output += "</div>"
 	}
-	output += "</body></html>"
-	return TemplateData{template.HTML(output)}
+	td.Body = template.HTML(output)
+	return
 }
 
 func main() {
