6e01ce1eb1d7a60e9c6f6216b6810c26245d71c7
Scott Worley <scottworley@scottworley.com>
2013-09-09 13:27:22 -0700

    Add a benchmark

---
+++ tl_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "bytes"
+import "io/ioutil"
+import "testing"
+
+func BenchmarkView(b *testing.B) {
+	buf, err := ioutil.ReadFile("testdata")
+	if err != nil {
+		b.Fatal("Could not read testdata", err)
+	}
+	reader := bytes.NewReader(buf)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		reader.Seek(0, 0)
+		view_handler(reader, ioutil.Discard)
+	}
+}
